40b720fe70f16f666a36867f4ed2c925beaab421
Francois Risch <risch.francois@gmail.com>
2025-03-27 15:48:22 +0100

    add source & target project names

--- move-experiments.sh
+++ move-experiments.sh
@@ -79,13 +79,13 @@ fi
 if [ "${TYPE}" == "export" ] ; then
 
     mkdir -p $WORK_DIR
-    mkdir -p ${WORK_DIR}/${SOURCE_PROJECT_NAME}
+    mkdir -p ${WORK_DIR}/source-${SOURCE_PROJECT_NAME}
 
     # 1. Get Project ID
     # curl -k -H "authorization: Bearer $API_KEY" -H "Accept: application/json"  https://workspace2.apps.mygpu-8.vpc.cloudera.com/api/v2/projects | jq
     S_PROJECT_ID=$(curl -k -H "authorization: Bearer $API_KEY" -H 'Accept: application/json' ${api_url}/projects | jq -r ".projects[] | select(.name == \"${SOURCE_PROJECT_NAME}\") | .id")
     echo "Project ID is: $S_PROJECT_ID"
-    echo "$S_PROJECT_ID" > ${WORK_DIR}/${SOURCE_PROJECT_NAME}/id
+    echo "$S_PROJECT_ID" > ${WORK_DIR}/source-${SOURCE_PROJECT_NAME}/id
 
 
     # # 2. Get List of Experiments
@@ -99,9 +99,9 @@ if [ "${TYPE}" == "export" ] ; then
         exp_name=$(echo $exp | jq -r '.name')
         echo "Treating exp: ${exp_name}"
 
-        mkdir -p ${WORK_DIR}/${SOURCE_PROJECT_NAME}/${exp_name}
-        echo $exp_id > ${WORK_DIR}/${SOURCE_PROJECT_NAME}/${exp_name}/id
-        echo $exp > ${WORK_DIR}/${SOURCE_PROJECT_NAME}/${exp_name}/def
+        mkdir -p ${WORK_DIR}/source-${SOURCE_PROJECT_NAME}/${exp_name}
+        echo $exp_id > ${WORK_DIR}/source-${SOURCE_PROJECT_NAME}/${exp_name}/id
+        echo $exp > ${WORK_DIR}/source-${SOURCE_PROJECT_NAME}/${exp_name}/def
 
         # # 2.1. Foreach Experiment, create a dir with id => Get all runs and foreach run, save it to a file run-$RUN_ID.json
         # curl -k -H "authorization: Bearer $API_KEY" -H "Accept: application/json"  https://workspace2.apps.mygpu-8.vpc.cloudera.com/api/v2/projects/ur3o-j4hq-ivcd-a7xy/experiments/ny6y-rs3g-8hdc-clgw/runs | jq
@@ -113,7 +113,7 @@ if [ "${TYPE}" == "export" ] ; then
         echo $RUN_IDS
         for run_id in $RUN_IDS ; do
             run_details=$(curl -k -H "authorization: Bearer $API_KEY" -H 'Accept: application/json' ${api_url}/projects/${S_PROJECT_ID}/experiments/${exp_id}/runs/${run_id})
-            echo $run_details > ${WORK_DIR}/${SOURCE_PROJECT_NAME}/${exp_name}/run-${run_id}.json
+            echo $run_details > ${WORK_DIR}/source-${SOURCE_PROJECT_NAME}/${exp_name}/run-${run_id}.json
         done
 
     done
@@ -123,11 +123,11 @@ elif [ "${TYPE}" == "import" ] ; then
 ### IMPORT
 
     mkdir -p $WORK_DIR
-    mkdir -p ${WORK_DIR}/${TARGET_PROJECT_NAME}
+    mkdir -p ${WORK_DIR}/target-${TARGET_PROJECT_NAME}
 
     export T_PROJECT_ID=$(curl -k -H "authorization: Bearer $API_KEY" -H 'Accept: application/json' ${api_url}/projects | jq -r ".projects[] | select(.name == \"${TARGET_PROJECT_NAME}\") | .id")
     echo "Project ID is: $T_PROJECT_ID"
-    echo "$T_PROJECT_ID" > ${WORK_DIR}/${TARGET_PROJECT_NAME}/id
+    echo "$T_PROJECT_ID" > ${WORK_DIR}/target-${TARGET_PROJECT_NAME}/id
 
     # 1. Foreach Experiment => Create a new experiment (and store its id)
     # curl -k -H "authorization: Bearer $API_KEY" -H "Accept: application/json" -XPOST  -d@experiment.json https://workspace2.apps.mygpu-8.vpc.cloudera.com/api/v2/projects/119e-1g6c-53ws-ggdr/experiments
@@ -137,20 +137,20 @@ elif [ "${TYPE}" == "import" ] ; then
     # "name": "test"
     # }
 
-    find ${WORK_DIR}/${SOURCE_PROJECT_NAME}/* -type d -print0 | while IFS= read -r -d $'\0' exp_dir ; do
+    find ${WORK_DIR}/source-${SOURCE_PROJECT_NAME}/* -type d -print0 | while IFS= read -r -d $'\0' exp_dir ; do
 
         exp_id=$( cat ${exp_dir}/id)
         export exp_name=$( basename $exp_dir )
         echo "Treating ${exp_name} with id: $exp_id"
 
-        mkdir -p ${WORK_DIR}/${TARGET_PROJECT_NAME}/${exp_name}
-        cp ${exp_dir}/def ${WORK_DIR}/${TARGET_PROJECT_NAME}/${exp_name}/
+        mkdir -p ${WORK_DIR}/target-${TARGET_PROJECT_NAME}/${exp_name}
+        cp ${exp_dir}/def ${WORK_DIR}/target-${TARGET_PROJECT_NAME}/${exp_name}/
 
         envsubst < exp_def.json > first_def.json
 
         CREATE_EXP=$(curl -k -H "authorization: Bearer $API_KEY" -H "Accept: application/json" -XPOST -d@first_def.json ${api_url}/projects/${T_PROJECT_ID}/experiments | jq )
         export exp_id_target=$(echo $CREATE_EXP | jq -r '.id')
-        echo $exp_id_target > ${WORK_DIR}/${TARGET_PROJECT_NAME}/${exp_name}/id
+        echo $exp_id_target > ${WORK_DIR}/target-${TARGET_PROJECT_NAME}/${exp_name}/id
         echo "Succesfully create experiment with id: $exp_id_target"
 
         rm -rf first_def.json
@@ -168,7 +168,7 @@ elif [ "${TYPE}" == "import" ] ; then
             # curl -k -H "authorization: Bearer $API_KEY" -H "Accept: application/json" -XPOST  -d@run.json https://workspace2.apps.mygpu-8.vpc.cloudera.com/api/v2/projects/119e-1g6c-53ws-ggdr/experiments/zpa6-ydgn-oecj-55ry/runs
 
             echo "treating run: $run_file"
-            cp $run_file ${WORK_DIR}/${TARGET_PROJECT_NAME}/${exp_name}/
+            cp $run_file ${WORK_DIR}/target-${TARGET_PROJECT_NAME}/${exp_name}/
             run_basename=$(basename $run_file)
             run_id=$( cat $run_file | jq -r '.id' )
 
@@ -182,9 +182,9 @@ elif [ "${TYPE}" == "import" ] ; then
             # # 2.1. Update the run
             # curl -k -H "authorization: Bearer $API_KEY" -H "Accept: application/json" -XPATCH  -d@run1.json https://workspace2.apps.mygpu-8.vpc.cloudera.com/api/v2/projects/119e-1g6c-53ws-ggdr/experiments/zpa6-ydgn-oecj-55ry/runs/bz94-p4hs-yf5r-9p6k
 
-            sed -i -e "s/${exp_id}/${exp_id_target}/" ${WORK_DIR}/${TARGET_PROJECT_NAME}/${exp_name}/${run_basename}
-            sed -i -e "s/${run_id}/${run_id_target}/" ${WORK_DIR}/${TARGET_PROJECT_NAME}/${exp_name}/${run_basename}
-            cp ${WORK_DIR}/${TARGET_PROJECT_NAME}/${exp_name}/${run_basename} run_full_def.json
+            sed -i -e "s/${exp_id}/${exp_id_target}/" ${WORK_DIR}/target-${TARGET_PROJECT_NAME}/${exp_name}/${run_basename}
+            sed -i -e "s/${run_id}/${run_id_target}/" ${WORK_DIR}/target-${TARGET_PROJECT_NAME}/${exp_name}/${run_basename}
+            cp ${WORK_DIR}/target-${TARGET_PROJECT_NAME}/${exp_name}/${run_basename} run_full_def.json
 
             curl -k -H "authorization: Bearer $API_KEY" -H "Accept: application/json" -XPATCH -d@run_full_def.json ${api_url}/projects/${T_PROJECT_ID}/experiments/${exp_id_target}/runs/${run_id_target}
             echo "Succesfully updated run with id: $run_id_target"
